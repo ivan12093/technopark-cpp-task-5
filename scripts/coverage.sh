f7d7043f0f41b93154348976c9ce2c37cda1ea43
Ivan <wanjasoldier@gmail.com>
2022-11-19 19:04:20 +0300

    default layout of project added.

---
+++ scripts/coverage.sh
@@ -0,0 +1,10 @@
+set -e
+export GTEST_COLOR=1
+export CTEST_OUTPUT_ON_FAILURE=true
+
+cd ../
+
+cmake -H. -Bbuild  -DBUILD_COVERAGE=ON
+cmake --build build
+cmake --build build --target test --verbose
+cmake --build build --target lcov
